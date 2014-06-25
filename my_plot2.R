2803eb1d47060369512abe763bf96ffb50e46a59
Karolis Uziela <karolis.uziela@scilifelab.se>
2014-06-25 15:15:37 +0200

    added my_plot2.R

---
+++ my_plot2.R
@@ -0,0 +1,51 @@
+library(tools)
+
+my_plot <- function(var1, var2, my_file, my_xlab, my_ylab, my_xlim=NULL, my_ylim=NULL, smooth=F) {
+    cor1 <- my_cor(var1, var2, "pearson")
+    cor2 <- my_cor(var1, var2, "spearman")
+    rmse <- my_rmse(var1, var2)
+    n <- length(var1)
+
+    my_ext <- file_ext(my_file)
+
+    if (my_file != "NONE") {
+        if (my_ext == "pdf") {
+            pdf(my_file, width=10, height=10)
+            #pdf(my_file)
+        } else {
+            png(my_file)
+        }
+    }
+    if (!smooth) {
+        plot(var1, var2, xlab=my_xlab, ylab=my_ylab, xlim=my_xlim, ylim=my_ylim)
+    } else {
+        if (is.null(my_ylim)) {
+            smoothScatter(var1, var2, xlab=my_xlab, ylab=my_ylab, xlim=my_xlim)
+        } else {
+            smoothScatter(var1, var2, xlab=my_xlab, ylab=my_ylab, xlim=my_xlim, ylim=my_ylim)
+        }
+    }
+    r = paste("pears =", cor1)
+    s = paste("spear =", cor2)
+    e = paste("rmse =", rmse)
+    n = paste("n =", n)
+
+    legend(x="topleft", legend=c(r, s, e, n), bg="white")
+
+    if (my_file != "NONE") {
+        dev.off()
+    }
+}
+
+my_cor <- function(var1, var2, my_method) {
+    cor_x <- cor(var1, var2, method=my_method)
+    cor_x <- round(cor_x, 2)
+    return(cor_x)
+}
+
+my_rmse <- function(var1, var2) {
+    rmse = sqrt(sum((var1 - var2) ^ 2) / length(var1))
+    rmse <- round(rmse, 2)
+    return(rmse)
+}
+
