afe97b0de58ae12e305c671e831b357fa3a97d2b
uziela <karolis.uziela@gmail.com>
2018-03-02 20:05:59 +0200

    Updated template.py with argparse

--- templates/template.py
+++ templates/template.py
@@ -3,63 +3,70 @@
 # Written by Karolis Uziela in 2018
 
 import sys
+import argparse
+import os
 
-################################ Usage ################################
+################################ Functions ####################################
 
-script_name = sys.argv[0]
+def get_arguments():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("input_file", help="Input file", type=str)
+    parser.add_argument("input_dir", help="Input directory", type=str)
+    parser.add_argument("output_file", help="Output file", type=str)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+    return parser.parse_args()
 
-usage_string = """
-Usage:
-
-    %s [Parameters]
-    
-Parameters:
-
-    <input-file> - input file
-    <output-file> - output file
-""" % script_name
-
-if len(sys.argv) != 3:
-    sys.exit(usage_string)
-
-################################ Functions ################################
-
-def read_data(input_file):
-    
-    with open(input_file) as f: 
+def read_file(input_file):
+    """Read file line by line and do... something"""
+    with open(input_file) as f:
         while True:
             line = f.readline()
-            if len(line) == 0: 
+            if len(line) == 0:
                 break
             line = line.rstrip('\n')
             #print(line)
-            #bits = line.split("\t")     
-        
-    
-def write_data(output_file, out_str):
+            #bits = line.split("\t")
+
+
+def write_file(output_file, out_str):
+    """Write out_str to output_file"""
     with open(output_file, "w") as f:
-        f.write(out_str)    
+        f.write(out_str)
+
+def list_directory(input_dir, ends_with=""):
+    """ List files in a directory 
+    Arguments:
+        input_dir -- directory to read
+        ends_with -- list only files whose filename end match "ends_with" pattern (optional)
+    """
+    files = []
+    for f in os.listdir(input_dir):
+        if os.path.isfile(os.path.join(input_dir, f)) and f.endswith(ends_with):
+            files.append(os.path.join(input_dir, f))
+    return files
 
 
-################################ Variables ################################
+###################### Global constants and Variables #########################
 
-# Input files/directories
-input_file = sys.argv[1]
-
-# Output files/directories
-output_file = sys.argv[2]
-
-# Constants
+# Global constants
 # N/A
 
 # Global variables
 # N/A
 
-################################ Main script ################################
+################################ Main script ##################################
     
-sys.stderr.write("%s is running with arguments: %s\n" % (script_name, str(sys.argv[1:])))
+if __name__ == '__main__':
+    args = get_arguments()
+    if args.verbose: 
+        sys.stderr.write("%s started running with arguments: %s\n" % (sys.argv[0], ' '.join(sys.argv[1:])))
 
-sys.stderr.write("%s done.\n" % script_name)
+    print(list_directory(args.input_dir, ends_with=".csv"))
+
+    if args.verbose:
+        sys.stderr.write("%s done.\n" % sys.argv[0])
 
 
 
---
+++ templates/template_old.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# Written by Karolis Uziela in 2018
+
+import sys
+
+################################ Usage ################################
+
+script_name = sys.argv[0]
+
+usage_string = """
+Usage:
+
+    %s [Parameters]
+    
+Parameters:
+
+    <input-file> - input file
+    <output-file> - output file
+""" % script_name
+
+if len(sys.argv) != 3:
+    sys.exit(usage_string)
+
+################################ Functions ################################
+
+def read_data(input_file):
+    
+    with open(input_file) as f: 
+        while True:
+            line = f.readline()
+            if len(line) == 0: 
+                break
+            line = line.rstrip('\n')
+            #print(line)
+            #bits = line.split("\t")     
+        
+    
+def write_data(output_file, out_str):
+    with open(output_file, "w") as f:
+        f.write(out_str)    
+
+
+################################ Variables ################################
+
+# Input files/directories
+input_file = sys.argv[1]
+
+# Output files/directories
+output_file = sys.argv[2]
+
+# Constants
+# N/A
+
+# Global variables
+# N/A
+
+################################ Main script ################################
+    
+sys.stderr.write("%s is running with arguments: %s\n" % (script_name, str(sys.argv[1:])))
+
+sys.stderr.write("%s done.\n" % script_name)
+
+
+
